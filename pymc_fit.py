885e45f78427ea4c35494b4af5dc4ecff604bbca
Jiacheng <zihutanejo0409@hotmail.com>
2017-09-27 20:08:18 -0700

    Fix hidden_vol model. Attempting a trial run on cluster with Theano.

--- pymc_fit.py
+++ pymc_fit.py
@@ -9,53 +9,47 @@ from datetime import datetime
 def now():  return str(datetime.now())[:-7]
 
 class ReadData:
-    def __init__(self, SplitYear=2000, yyyymmdd_colname = 'caldt'):
-        file_loc = r"https://raw.githubusercontent.com/jiacheng0409/mcmc_sv/master/sp_daily.csv"
+    def __init__(self, StartYear = 1962, EndYear=1999):
+        file_loc = r"https://raw.githubusercontent.com/jiacheng0409/mcmc_sv/master/sp500_196201_201612.csv"
         rwData = pd.read_csv(file_loc)
-        yyyymmdd = rwData[yyyymmdd_colname]
+        yyyymmdd = rwData['DATE']
         df_norm = (rwData - rwData.mean()) /rwData.std()
-        df_norm[yyyymmdd_colname] = yyyymmdd
+        df_norm['DATE'] = yyyymmdd
 
-        temp = df_norm['sprtrn'].shift(periods=1)
-        temp.iloc[0] = df_norm['sprtrn'].iloc[0].copy()
-        df_norm['sprtrn_lag'] = temp
+        temp = df_norm['logret'].shift(periods=1)
+        temp.iloc[0] = df_norm['logret'].iloc[0].copy()
+        df_norm['logret_lag'] = temp
 
-        train_IDX = df_norm[yyyymmdd_colname] <= SplitYear * (10 ** 4)
+        train_IDX = (df_norm['DATE'] <= (EndYear+1) * (10 ** 4)) & (df_norm['DATE'] >= StartYear * (10 ** 4))
         self.train = df_norm[train_IDX]
         self.test = df_norm[~train_IDX]
         print('[INFO] Finished data importing.')
 
 def exponential_model(training_data_df):
-    returns = training_data_df['sprtrn'].as_matrix()
-
+    logreturns = training_data_df['logret'].as_matrix()
     with pm.Model() as model_obj:
         nu = pm.Exponential('nu', 1./10, testval=5.)
         sigma = pm.Exponential('sigma', 1./.02, testval=.1)
-        s = pm.GaussianRandomWalk('s', sigma**-2, shape=len(returns))
+        s = pm.GaussianRandomWalk('s', sigma**-2, shape=len(logreturns))
         volatility_process = pm.Deterministic('volatility_process', pm.math.exp(-2*s))
-        r = pm.StudentT('r', nu, lam=1/volatility_process, observed=returns)
+        r = pm.StudentT('r', nu, lam=1/volatility_process, observed=logreturns)
     return model_obj
 
 def hidden_vol_model(training_data_df):
-    returns = training_data_df['vwretd'].as_matrix()
-    returns_lag = training_data_df['vwretd_lag'].as_matrix()
-    tbill_lag = training_data_df['tbill_lag'].as_matrix()
+    logreturns = training_data_df['logret'].as_matrix()
+    logreturns_lag = training_data_df['logret_lag'].as_matrix()
 
     with pm.Model() as model_obj:
-        n_obs = returns.shape[0]
+        n_obs = logreturns.shape[0]
 
         epsilon = pm.Normal(name='epsilon', sd=1.0, mu=.0, shape=n_obs)
-        beta_vec = pm.Normal(name='beta_vec', sd=1.0, mu=.0, shape=3)
+        beta_vec = pm.Normal(name='beta_vec', sd=1.0, mu=.0, shape=2)
 
         alpha_0 = pm.HalfNormal(name='alpha_0',sd=1.0, shape=1)
         ar_1_para = pm.Bound(pm.Normal, lower=-1.0 + 1e-6, upper=1.0 - 1e-6)
         alpha_1 = ar_1_para('alpha_1', mu=0, sd=1)
 
-        # sigma_nu = 1.0
-        # sigma_inv = pm.ChiSquared(name='sigma_inv', nu=sigma_nu)
-        # sigma_sq = pm.Deterministic(name='sigma', var=sigma_nu/sigma_inv)
         sigma_sq = pm.InverseGamma(name='sigma', alpha=2.0, shape=1)
-        # v = pm.Normal(name='v',sd=pm.math.sqrt(sigma_sq),shape=n_obs)
 
         ln_h_list = list()
         r_list = list()
@@ -74,15 +68,15 @@ def hidden_vol_model(training_data_df):
 
             ln_h_list.append(this_ln_h)
             this_r = pm.Normal(name='r_{}'.format(t),
-                               mu=beta_vec[0] + tbill_lag[t]*beta_vec[1]+ returns_lag[t]*beta_vec[2],
+                               mu=beta_vec[0] + logreturns_lag[t]*beta_vec[1],
                                sd=pm.math.sqrt(this_h),
-                               observed=returns[t])
+                               observed=logreturns[t])
             r_list.append(this_r)
     print('[INFO] hidden volatility model built.')
     return model_obj
 
-def main(split_year, n_draw, model):
-    data = ReadData(split_year)
+def main(StartYear, EndYear, n_draw, model):
+    data = ReadData(StartYear, EndYear)
     training_data_df = data.train
 
     if model == 'exponential':
@@ -108,11 +102,12 @@ if __name__ == '__main__':
     # Initialize random number generator
     np.random.seed(123)
     print('[INFO] Code starts running at {}.\n'.format(now()))
-    split_year = 2013
-    n_draw = 2000
+    StartYear = 1962
+    EndYear = 1999
+    n_draw = 200
 
     # Already implemented models:
     # 'exponential' : from 'http://docs.pymc.io/notebooks/stochastic_volatility.html?highlight=stochastic%20volatility'
     # 'hidden_vol': from Section 10.7 [Tsay; 2002]
     chosen_model = parser.parse_args().model
-    main(split_year, n_draw, chosen_model)
+    main(StartYear, EndYear, n_draw, chosen_model)
